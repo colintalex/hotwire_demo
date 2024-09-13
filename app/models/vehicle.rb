29cc6b11a987f4902afe45411d8aad2f343c96a6
calexander-sanborn <calexander@sanborn.com>
2024-09-13 11:00:26 -0600

    Working

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,16 @@
 class Vehicle < ApplicationRecord
-
-  after_update_commit :update_message_count
+  after_update_commit :update_vehicle_info
+  after_destroy_commit :remove_vehicle_info
 
   private
 
-  def update_message_count
+  def update_vehicle_info
     dom_id = ActionView::RecordIdentifier.dom_id(self, :info)
     broadcast_update_to(dom_id, target: dom_id, partial: "vehicles/vehicle", locals: { vehicle: self })
+  end
+
+  def remove_vehicle_info
+    dom_id = ActionView::RecordIdentifier.dom_id(self, :info)
+    broadcast_update_to(dom_id, target: dom_id, partial: "vehicles/removed")
   end
 end
